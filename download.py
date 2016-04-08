a57a7ea1ae575fad3c02cc71cb6ecf5b702a68ab
siznax <steve@siznax.net>
2016-04-08 16:46:24 -0700

    flake8 fixes

--- download.py
+++ download.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python2.7
 
-__author__ = "siznax"
-__version__ = "Jun 2014"
+from __future__ import print_function
 
 import argparse
 import json
@@ -9,54 +8,47 @@ import os
 import pycurl
 import sys
 
-
-# def write_text_file(fname, data):
-#   if not os.path.exists(fname):
-#     with open(fname, 'w') as fp:
-#       fp.write(data['txt'].encode('utf-8') + "\n")
-#       fp.write(data['href'] + "\n")
-#       print "  wrote %d byes to %s" % (fp.tell(), fname)
-#   else:
-#     print "  " + fname
-#   sys.stdout.flush()
+__author__ = "siznax"
+__version__ = "Jun 2014"
 
 
 def write_movie_file(fname, data):
-  if not os.path.exists(fname):
-    with open(fname, 'wb') as fp:
-      print "CURL " + data['movie']
-      curl = pycurl.Curl()
-      curl.setopt(pycurl.URL, str(data['movie']))
-      curl.setopt(pycurl.WRITEDATA, fp)
-      curl.perform()
-      curl.close()
-      print "  wrote %d byes to %s" % (fp.tell(), fname)
-  else:
-    print "  " + fname
-  sys.stdout.flush()
+    if not os.path.exists(fname):
+        with open(fname, 'wb') as fp:
+            print("GET " + data['movie'], file=sys.stderr)
+            curl = pycurl.Curl()
+            curl.setopt(pycurl.URL, str(data['movie']))
+            curl.setopt(pycurl.WRITEDATA, fp)
+            curl.perform()
+            curl.close()
+            print("+ wrote %d byes to %s" % (fp.tell(), fname),
+                  file=sys.stderr)
+    else:
+        print("  " + fname, file=sys.stderr)
+    sys.stdout.flush()
 
 
 def download(item, dest):
-  url = item['movie']
-  movie_file = url.split('/')[-1]
-  write_movie_file(os.path.join(dest, movie_file), item)
-  # text_file = movie_file.replace(os.path.splitext(movie_file)[-1], '.txt')
-  # write_text_file(os.path.join(dest, text_file), def)
+    url = item['movie']
+    movie_file = url.split('/')[-1]
+    write_movie_file(os.path.join(dest, movie_file), item)
 
 
 def main(args):
-  if not os.path.exists(args.data_file):
-    print "data_file not found: " + args.data_file
-    sys.exit(os.EX_USAGE)
+    if not os.path.exists(args.data_file):
+        print("data_file not found: " + args.data_file, file=sys.stderr)
+        sys.exit(os.EX_USAGE)
 
-  data = json.load(open(args.data_file))
-  for item in data:
-    download(item, args.dest)
+    data = json.load(open(args.data_file))
+    for item in data:
+        download(item, args.dest)
 
 
 if __name__ == "__main__":
-  """download movie files for upload to archive.org"""
-  argp = argparse.ArgumentParser()
-  argp.add_argument('dest', help='destination path')
-  argp.add_argument('data_file', help='JSON output from crawl.py (CrawlBasho)')
-  main(argp.parse_args())
+    """download movie files for upload to archive.org"""
+    argp = argparse.ArgumentParser()
+    argp.add_argument('dest',
+                      help='destination path')
+    argp.add_argument('data_file',
+                      help='JSON output from crawl.py (CrawlBasho)')
+    main(argp.parse_args())
