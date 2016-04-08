a16c6b6ebdbb579146359edb6fb7277d45d0b00c
siznax <steve@siznax.net>
2016-04-08 13:42:11 -0700

    flake8 fix index.py

--- index.py
+++ index.py
@@ -34,6 +34,7 @@ body {
 """
 DETAILS = 'https://archive.org/details'
 DOWNLOAD = 'https://archive.org/download'
+
 HTML_TEMPLATE = Template("""<!doctype html>
 <head>
 <meta charset="utf-8">
@@ -43,24 +44,26 @@ HTML_TEMPLATE = Template("""<!doctype html>
 </head>
 <h1>${title}</h1>
 """)
-MOVIE_DIV = Template("""<div class=movie>
+
+MOVIE_DIV = Template("""<div id="${num}" class="movie">
 ${text}
 ${video}
 </div>
 """)
+
 TAIL = Template("""<p>
 Source: <a href="${source}">${source}</a><br>
 Archive: <a href="${details}/${archive}">${details}/${archive}</a><br>
 Code: <a href="${code}">${code}</a><br>
 Date: ${date}<br>
 </p>""")
-TEXT_TEMPLATE = Template("""<p>
-<b>Highlight ${hnum}</b>
-<p id="jp">${jp}</p>
-<p id="en">${en}</p>
-</p>""")
-VIDEO_TEMPLATE = Template("""<video controls preload="none"
- poster="${poster}">
+
+TEXT_TEMPLATE = Template("""<p><b>Highlight ${num}</b></p>
+<p class="jp">${jp}</p>
+<p class="en">${en}</p>""")
+
+VIDEO_TEMPLATE = Template("""<video
+ controls preload="none" poster="${poster}">
  <source src="${mp4}" type="video/mp4">
  <source src="${ogg}" type="video/ogg">
 Your browser does not support the video tag.
@@ -68,57 +71,58 @@ Your browser does not support the video tag.
 
 
 def print_divs(item, archive):
-  mp4 = item['movie'].split('/')[-1]
-  ogg = mp4.replace('.mp4', '.ogg')
-  poster = mp4.replace('.mp4', '.gif')
-  mp4_url = "%s/%s/%s" % (DOWNLOAD, archive, mp4)
-  ogg_url = "%s/%s/%s" % (DOWNLOAD, archive, ogg)
-  poster_url = "%s/%s/%s" % (DOWNLOAD, archive, poster)
-  video = VIDEO_TEMPLATE.substitute(
-    poster=poster_url,
-    mp4=mp4_url, ogg=ogg_url)
-  text = TEXT_TEMPLATE.substitute(
-    hnum=int(os.path.splitext(mp4)[0].split('_')[-1]),
-    jp=item['txt']['jp'],
-    en=item['txt']['en'])
-  print MOVIE_DIV.substitute(video=video, text=text).encode('utf-8')
+    mp4 = item['movie'].split('/')[-1]
+    ogg = mp4.replace('.mp4', '.ogg')
+    poster = mp4.replace('.mp4', '.gif')
+    mp4_url = "%s/%s/%s" % (DOWNLOAD, archive, mp4)
+    ogg_url = "%s/%s/%s" % (DOWNLOAD, archive, ogg)
+    poster_url = "%s/%s/%s" % (DOWNLOAD, archive, poster)
+    num = int(os.path.splitext(mp4)[0].split('_')[-1])
+    video = VIDEO_TEMPLATE.substitute(poster=poster_url,
+                                      mp4=mp4_url, ogg=ogg_url)
+    text = TEXT_TEMPLATE.substitute(num=num,
+                                    jp=item['txt']['jp'],
+                                    en=item['txt']['en'])
+    print MOVIE_DIV.substitute(num=num, video=video, text=text).encode('utf-8')
 
 
 def main(config):
+    if not os.path.exists(config["data_file"]):
+        print "ERROR data_file not found: " + config["data_file"]
+        sys.exit(os.EX_NOINPUT)
+    title = config["title"].encode("utf-8").replace("Grand", "<br>Grand")
+    print HTML_TEMPLATE.substitute(title=title, css=CSS)
 
-  if not os.path.exists(config["data_file"]):
-    print "ERROR data_file not found: " + config["data_file"]
-    sys.exit(os.EX_NOINPUT)
-  title = config["title"].encode("utf-8").replace("Grand", "<br>Grand")
-  print HTML_TEMPLATE.substitute(title=title, css=CSS)
+    with open(config["data_file"]) as fp:
+        data = json.loads(fp.read())
+        for item in data:
+            print_divs(item, config["archive"])
 
-  with open(config["data_file"]) as fp:
-    data = json.loads(fp.read())
-    for item in data:
-      print_divs(item, config["archive"])
+    print TAIL.substitute(source=config["source"],
+                          date=config["date"],
+                          details=DETAILS,
+                          archive=config["archive"],
+                          acode=config["code"])
+    sys.exit(os.EX_OK)
 
-  print TAIL.substitute(source=config["source"],
-                        date=config["date"],
-                        details=DETAILS,
-                        archive=config["archive"],
-                        code=config["code"])
-  sys.exit(os.EX_OK)
 
 if __name__ == "__main__":
-  argp = argparse.ArgumentParser()
-  argp.add_argument('data_file', help='JSON output from crawl.py (CrawlBasho)')
-  argp.add_argument('selector', help='config.json selector')
-  args = argp.parse_args()
+    argp = argparse.ArgumentParser()
+    argp.add_argument('data_file',
+                      help='JSON output from crawl.py (CrawlBasho)')
+    argp.add_argument('selector',
+                      help='config.json selector')
+    args = argp.parse_args()
 
-  config_file = "config.json"
+    config_file = "config.json"
 
-  if os.path.exists(config_file):
-    with open(config_file) as fp:
-      cfg_json = json.loads(fp.read())
-      config = cfg_json[args.selector]
-      config["data_file"] = args.data_file
-      config["code"] = cfg_json["code"]
-      main(config)
+    if os.path.exists(config_file):
+        with open(config_file) as fp:
+            cfg_json = json.loads(fp.read())
+            config = cfg_json[args.selector]
+            config["data_file"] = args.data_file
+            config["code"] = cfg_json["code"]
+            main(config)
 
-  print "ERROR config file not found: " + config_file
-  sys.exit(os.EX_NOINPUT)
+    print "ERROR config file not found: " + config_file
+    sys.exit(os.EX_NOINPUT)
